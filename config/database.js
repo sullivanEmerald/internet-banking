44141c72d9abb8dbf7d0d5063032c472a053645d
Sullivan Amadike <amadikesullivan20@gmail.com>
2023-06-14 17:51:15 +0100

    added strictQuery for mongoose connection

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+mongoose.set("strictQuery", true);
+
 const connectDB = async () => {
     try {
        const conn = await mongoose.connect(process.env.DB_STRING, {
